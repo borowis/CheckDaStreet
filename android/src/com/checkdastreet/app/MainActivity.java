5d270435285960cf8257bb7ca010a38f33eabf31
vklimkov <slavka470@gmail.com>
2014-09-13 21:34:37 +0300

    Added basic UI implementation 2

--- android/src/com/checkdastreet/app/MainActivity.java
+++ android/src/com/checkdastreet/app/MainActivity.java
@@ -1,111 +1,216 @@
 package com.checkdastreet.app;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.*;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
-import android.media.CamcorderProfile;
-import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
+import android.view.*;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    SurfaceView surfaceView;
-    Camera camera;
-
-    String LOG_TAG = "camera_debug";
+    ImageButton capture, recapture, save;
+    CameraPreview preview;
+    Camera mCamera;
+    FrameLayout mFrame;
+    LinearLayout mButtons;
+    Context mContext;
+    Uri fileUri;
+    Intent intent;
+    byte[] pictureData;
 
     File photoFile;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        intent = getIntent();
+        Log.i("MY", intent.getAction());
+        mContext = this;
+
         File pictures = Environment
                 .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
         photoFile = new File(pictures, "myphoto.jpg");
 
+        // кнопки
+        mButtons = (LinearLayout) findViewById(R.id.buttons);
 
-        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
-
-        SurfaceHolder holder = surfaceView.getHolder();
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        holder.addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                try {
-                    camera.setPreviewDisplay(holder);
-                    camera.startPreview();
-                } catch (Exception e) {
-                    StringWriter sw = new StringWriter();
-                    PrintWriter pw = new PrintWriter(sw);
-                    e.printStackTrace(pw);
-                    sw.toString();
-
-                    Log.d(LOG_TAG, e.toString());
-                    Log.d(LOG_TAG, sw.toString());
-
-                    e.printStackTrace();
+        capture = (ImageButton) findViewById(R.id.capture);
+        capture.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mCamera.takePicture(null, null, null, mPictureCallback);
+                    }
                 }
-            }
+        );
 
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format,
-                                       int width, int height) {
-            }
+        recapture = (ImageButton) findViewById(R.id.recapture);
+        recapture.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        hideConfirm();
+                        mCamera.startPreview();
+                        pictureData = null;
+                    }
+                }
+        );
 
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-            }
-        });
+        save = (ImageButton) findViewById(R.id.finish);
+        save.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Uri pictureFile;
+
+                        if (intent.hasExtra(MediaStore.EXTRA_OUTPUT)) {
+                            pictureFile = (Uri)intent.getExtras().getParcelable(MediaStore.EXTRA_OUTPUT);
+                        }
+                        else
+                            pictureFile = generateFile();
+
+                        try {
+                            savePhotoInFile (pictureData, pictureFile);
+                            intent.putExtra("data", pictureData);
+                            intent.setData(pictureFile);
+                            setResult(RESULT_OK, intent);
+                        } catch (Exception e) {
+                            setResult(2, intent);
+                        }
+                        mCamera.release();
+                        finish();
+                    }
+                }
+        );
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        try {
-            camera = Camera.open();
-        } catch (Exception e){
-            Log.d(LOG_TAG, e.toString());
+    private Camera openCamera() {
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
+            return null;
+
+        Camera cam = null;
+        if (Camera.getNumberOfCameras() > 0) {
+            try {
+                cam = Camera.open(0);
+            }
+            catch (Exception exc) {
+                //
+            }
         }
-        Log.d(LOG_TAG, "Camera Open");
+
+        return cam;
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (camera != null)
-            camera.release();
-        camera = null;
-    }
+    private PictureCallback mPictureCallback = new PictureCallback() {
 
-    public void onClickPicture(View view) {
-        camera.takePicture(null, null, new PictureCallback() {
-            @Override
-            public void onPictureTaken(byte[] data, Camera camera) {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            if (intent.getAction().equals (MediaStore.ACTION_IMAGE_CAPTURE)) {
+                pictureData = data;
+                showConfirm ();
+            }
+            else {
+                Uri pictureFile = generateFile();
                 try {
+                    //SAVE TO FILE
+
                     FileOutputStream fos = new FileOutputStream(photoFile);
                     fos.write(data);
                     fos.close();
 
 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
 
+                    Toast.makeText(mContext, "Save file: " + photoFile.getName(), Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+
+                    Toast.makeText(mContext, "Error: can't save file", Toast.LENGTH_LONG).show();
+                }
+                mCamera.startPreview();
+            }
+        }
+    };
+
+    private void showConfirm() {
+        capture.setVisibility(View.INVISIBLE);
+        mButtons.setVisibility(View.VISIBLE);
     }
 
+    private void hideConfirm() {
+        mButtons.setVisibility(View.INVISIBLE);
+        capture.setVisibility(View.VISIBLE);
+    }
+
+    private void savePhotoInFile(byte[] data, Uri pictureFile) throws Exception {
+
+        if (pictureFile == null)
+            throw new Exception();
+
+        OutputStream os = getContentResolver().openOutputStream(pictureFile);
+        os.write(data);
+        os.close();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mCamera != null){
+            mCamera.stopPreview();
+            mCamera.release();
+            mFrame.removeView(preview);
+            mCamera = null;
+            preview = null;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCamera = openCamera ();
+        if (mCamera == null) {
+            Toast.makeText(this, "Opening camera failed", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        preview = new CameraPreview(this, mCamera);
+        mFrame = (FrameLayout) findViewById(R.id.layout);
+        mFrame.addView(preview, 0);
+    }
+
+    private Uri generateFile() {
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED))
+            return null;
+
+        File path = new File (Environment.getExternalStorageDirectory(), "CameraTest");
+        if (! path.exists()){
+            if (! path.mkdirs()){
+                return null;
+            }
+        }
+
+        String timeStamp = String.valueOf(System.currentTimeMillis());
+        File newFile = new File(path.getPath() + File.separator + timeStamp + ".jpg");
+        return Uri.fromFile(newFile);
+    }
 }
