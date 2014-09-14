93765b4e82bb57150e9a77b256a1ae9832c7ad59
Borys Zibrov <bzibrov@archer-soft.com>
2014-09-14 03:46:06 +0300

    modify image cropping tool a bit

--- image_cropping_tool/src/main/java/FileChooser.java
+++ image_cropping_tool/src/main/java/FileChooser.java
@@ -1,3 +1,4 @@
+import java.awt.im.InputContext;
 import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -136,5 +137,24 @@ public class FileChooser extends JPanel
                                            createAndShowGUI();
                                        }
                                    });
+
+        AWTEventListener awtWindowListener = new AWTEventListener() {
+            @Override
+            public void eventDispatched(AWTEvent event) {
+                if (event instanceof WindowEvent) {
+                    if (WindowEvent.WINDOW_CLOSED == event.getID()
+                            || WindowEvent.WINDOW_CLOSING == event.getID()) {
+                        Window child = ((WindowEvent) event).getWindow();
+                        Window parent = SwingUtilities.getWindowAncestor(child);
+                        if (parent == null) return;
+                        InputContext childIC = child.getInputContext();
+                        parent.getInputContext().selectInputMethod(childIC.getLocale());
+                    }
+                }
+
+            }
+        };
+
+        Toolkit.getDefaultToolkit().addAWTEventListener(awtWindowListener, AWTEvent.WINDOW_EVENT_MASK);
     }
 }
